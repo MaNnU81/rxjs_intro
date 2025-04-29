dcc4c6f625cc49b7a8844e6c8b68ff581f7be459
MaNnu81 <mannuspamm@gmail.com>
2025-04-29 10:20:10 +0200

    fine project

--- src/app/components/slave/slave.component.ts
+++ src/app/components/slave/slave.component.ts
@@ -1,19 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { ComService } from '../../services/com/com.service';
+import { AsyncPipe } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-slave',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './slave.component.html',
   styleUrl: './slave.component.scss'
 })
 export class SlaveComponent {
 
+
 comService = inject(ComService);
+
+normalSubscription?: Subscription;
 
 
 subscribeResbj() {
-
+  this.comService.resbj.subscribe(data => console.log('replay subject', data));
 }
 
 subscribeBesbj() {
@@ -21,7 +26,11 @@ subscribeBesbj() {
 }
 
 subscribeSbj() {
-this.comService.resbj.subscribe(data => console.log('replay subject', data));
+this.normalSubscription = this.comService.sbj.subscribe(data => console.log('normal subject', data));
 }
+
+unsubscribe() {
+  this.normalSubscription?.unsubscribe();
+  }
 
 }
